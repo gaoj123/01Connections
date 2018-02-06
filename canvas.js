5f2cb6c9a895c1b8f047024d70b24e4791fe1a2f
Jenny Gao <jgao658@gmail.com>
2018-02-05 19:37:42 -0500

    working

--- canvas.js
+++ canvas.js
@@ -1,26 +1,40 @@
 var clearButton=document.getElementById("clear")
-var toggleButton=document.getElementById("toggle")
-
 var canvas=document.getElementById("slate");
 var ctx=canvas.getContext("2d");
 
-ctx.beginPath();
+var started=false;
 
 var circle=function(e){
     mouseX=e.offsetX;
     mouseY=e.offsetY;
+    started=true;
+    ctx.beginPath();
     ctx.arc(mouseX,mouseY,10,0,2*Math.PI);
     ctx.fillStyle="blue";
     ctx.fill();
     ctx.stroke();
+    ctx.moveTo(mouseX,mouseY);
 };
 
 var clear=function(e){
     ctx.clearRect(0,0,500,500);
+    ctx.beginPath();
 }
 
+var draw=function(e){
+    if (started){
+	mouseX=e.offsetX;
+	mouseY=e.offsetY;
+	ctx.lineTo(mouseX,mouseY);
+	ctx.stroke();
+	circle(e);
+    }
+    else{
+	circle(e);
+    }
+};
     
-canvas.addEventListener("click",circle);
+canvas.addEventListener("click",draw);
 clearButton.addEventListener("click", clear);
 
 
